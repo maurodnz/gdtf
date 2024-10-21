f17072e6707cc9e774c29fa7407b97cc059681fb
Mauro <m.danzo@makerweb.it>
2024-10-21 14:41:31 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # gdtf
 Basic PHP library for interfacing with the General Device Type Format
+
+## Installation
+Add these lines to your `composer.json` file
+
+```
+{
+    "repositories": [
+        {
+            "type": "vcs",
+            "url": "https://github.com/tuo-utente/tuo-modulo.git"
+        }
+    ],
+    "require": {
+        "tuo-utente/tuo-modulo": "dev-main"
+    }
+}
+```
+
+then execute `composer install` to add gdtf to your project.
---
+++ src/Gdtf.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace maurodnz\GDTF;
+
+use Exception;
+use ZipArchive;
+
+class GDTF
+{
+    private $filepath;
+    private $xml;
+
+    public function __construct(string $path)
+    {
+        $this->filepath = $path;
+
+        if (!file_exists($path) || pathinfo($path, PATHINFO_EXTENSION !== 'gdtf')) {
+            throw new Exception("GDTF file is invalid or not found.");
+        }
+
+        $this->xml = $this->extract_definition_xml();
+    }
+
+    public function get_content()
+    {
+        return $this->xml;
+    }
+
+    private function extract_definition_xml()
+    {
+        $zip = new ZipArchive();
+
+        if ($zip->open($this->filepath) === true) {
+            $xml_index = $zip->locateName("description.xml");
+
+            if ($xml_index === false) {
+                throw new Exception("description.xml file missing.");
+            }
+
+            $content = $zip->getFromIndex($xml_index);
+            $zip->close();
+
+            return $content;
+        } else {
+            throw new Exception("Unable to open GDTF archive.");
+        }
+    }
+}
